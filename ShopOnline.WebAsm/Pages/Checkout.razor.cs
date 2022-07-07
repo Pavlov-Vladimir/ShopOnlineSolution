b979533bc94e9aaec00ccba2ea3d9591be8a0d66
Vladimir Pavlov <bob.pavlov.a7@gmail.com>
2022-07-07 17:16:27 +0300

    Added Checkout page. Integrated PayPal Payment.

---
+++ ShopOnline.WebAsm/Pages/Checkout.razor.cs
@@ -0,0 +1,51 @@
+﻿namespace ShopOnline.WebAsm.Pages;
+
+public partial class Checkout
+{
+    [Inject]
+    public IJSRuntime JS { get; set; } = null!;
+    [Inject]
+    public IShoppingCartService ShoppingCartService { get; set; } = null!;
+    protected IEnumerable<CartItemDto>? CartItems { get; set; }
+    protected int TotalQty { get; set; }
+    protected string? PaymentDescription { get; set; }
+    protected decimal PaymentAmount { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            CartItems = await ShoppingCartService.GetItems(HardCoded.UserId);
+
+            if (CartItems is not null)
+            {
+                Guid orderGuid = Guid.NewGuid();
+
+                PaymentAmount = CartItems.Sum(x => x.TotalPrice);
+                TotalQty = CartItems.Sum(x => x.Qty);
+                PaymentDescription = $"O_{HardCoded.UserId}_{orderGuid}";
+            }
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        try
+        {
+            if (firstRender)
+            {
+                await JS.InvokeVoidAsync("initPayPalButton");
+            }
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+}
